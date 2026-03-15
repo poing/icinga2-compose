9efb0047ff5034edc8eb3a02fb57825b3f186a67
poing <brian.lavallee@invite-comm.jp>
2026-03-15 12:43:54 +0900

    setting up this repo

--- i2m2/setup.sh
+++ i2m2/setup.sh
@@ -35,10 +35,9 @@ sync_ca_files() {
         echo "Restart the container and run setup.sh AGAIN!."
     else
         echo "Files match. No action required."
-        #update_icinga2_config
-        #update_icinga2_ticket
+        icinga2 node wizard
+        update_icinga2_config 
     fi
-
 
 }
 
@@ -46,23 +45,11 @@ sync_ca_files() {
 update_icinga2_config() {
     echo "Updating Icinga2 configuration..."
     sudo rm -rf /etc/icinga2
-    sudo ln -sv /tmp/i2m2/etc /etc/icinga2 > /dev/null
-    echo "Restart the container.  Setup of i2m1 is complete."
+    sudo ln -sv /mnt/i2m2/etc /etc/icinga2 > /dev/null
+    echo "Restart the container.  Setup of i2m2 is complete."
 }
-
-update_icinga2_ticket() {
-
-    TICKET_FILE="/mnt/master/i2m2"  # Change this to your actual file path
-
-    # Read the ticket value from the file
-    TICKET=$(<"$TICKET_FILE")
-    icinga2 node setup --zone master --parent_host i2m1 --endpoint i2m2 --ticket "$TICKET" --trustedcert /mnt/master/i2m1.crt 
-    
-}
-
 
 # Execute functions
 sync_ca_files
-#update_icinga2_config
 
 
--- i2s2/setup.sh
+++ i2s2/setup.sh
@@ -35,10 +35,7 @@ sync_ca_files() {
         echo "Restart the container and run setup.sh AGAIN!."
     else
         echo "Files match. No action required."
-        #update_icinga2_config
-        #update_icinga2_ticket
     fi
-
 
 }
 
@@ -46,25 +43,10 @@ sync_ca_files() {
 update_icinga2_config() {
     echo "Updating Icinga2 configuration..."
     sudo rm -rf /etc/icinga2
-    sudo ln -sv /tmp/i2s2/etc /etc/icinga2 > /dev/null
+    sudo ln -sv /mnt/i2s2/etc /etc/icinga2 > /dev/null
     echo "Restart the container.  Setup of i2m1 is complete."
 }
 
-update_icinga2_ticket() {
-
-    TICKET_FILE="/mnt/master/i2m2"  # Change this to your actual file path
-
-    # Read the ticket value from the file
-    TICKET=$(<"$TICKET_FILE")
-    icinga2 node setup --zone master --parent_host i2m1 --endpoint i2m2 --ticket "$TICKET" --trustedcert /mnt/master/i2m1.crt 
-    
-}
-
-icinga2 node wizard
-update_icinga2_config 
-
 # Execute functions
-#sync_ca_files
-#update_icinga2_config
-
-
+icinga2 node wizard
+update_icinga2_config
